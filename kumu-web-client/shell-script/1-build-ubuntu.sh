eae962c1e3e97adc5da5ade17c82ad47b148e487
kum_cp <kum_cp@yahoo.com.vn>
2021-12-13 13:37:28 +0700

    update script

--- kumu-web-client/shell-script/1-build-ubuntu.sh
+++ kumu-web-client/shell-script/1-build-ubuntu.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This script is built for ubuntu 20.04
+
 sudo apt update
 sudo apt install curl -y
 
@@ -9,7 +11,7 @@ echo " ==== Installing NGINX ===="
 # Install and configure web service
 sudo apt install nginx -y
 
-sed -i -E 's+root /var/www/html;+root /home/ubuntu/kumu/kumu-web-client/dist;+g' /etc/nginx/sites-enabled/default
+sed -i -E 's+root /var/www/html;+root /var/www/html/kumu/kumu-web-client/dist;+g' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 
 
@@ -18,6 +20,10 @@ echo " ==== Installing NVM ===="
 cd /var/www/html
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
+
+export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo " ==== Installing NodeJS ===="
 nvm install 12.20.2
